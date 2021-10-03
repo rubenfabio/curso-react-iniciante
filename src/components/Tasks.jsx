d2444cdda2144f61d2f63fa09a8d3f9b742d873a
Ruben Fabio Gutierrez Gongora <ruben.fabio2016@gmail.com>
2021-10-03 11:07:50 -0300

    update

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Task from "./Task";
+
+const Tasks = ({ tasks, handleTaskClick, handleTaskDelete }) => {
+    return (
+        <>
+            { tasks.map((task) => (
+                <Task 
+                key={task.id}
+                task={task} 
+                handleTaskClick={handleTaskClick} 
+                handleTaskDelete={handleTaskDelete} />
+            ))}
+        </>
+    );
+};
+
+export default Tasks;   
